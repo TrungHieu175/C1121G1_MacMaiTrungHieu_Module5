b0d944841b21238995ad038b30b25ec3ae9ce900
TrungHieu175 <ablohaha01@gmail.com>
2022-05-04 17:31:54 +0700

    add

---
+++ ss2/bai_tap/fibonaci/src/app/fibonaci.js
@@ -0,0 +1,16 @@
+function calculateSum(n) {
+    var fibo = [];
+    if (n <= 0) {
+        return 0;
+    }
+    fibo[0] = 0;
+    fibo[1] = 1;
+    var sum = fibo[0] + fibo[1];
+    for (var i = 2; i <= n; i++) {
+        fibo[i] = fibo[i - 1] +
+            fibo[i - 2];
+        sum += fibo[i];
+    }
+    return sum;
+}
+console.log(calculateSum(15));
