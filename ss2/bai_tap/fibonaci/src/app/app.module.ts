34de9a4a57324475ae59d4e66d5f116603b81a02
TrungHieu175 <ablohaha01@gmail.com>
2022-05-09 18:00:59 +0700

    add

--- ss2/bai_tap/fibonaci/src/app/app.module.ts
+++ ss2/bai_tap/fibonaci/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ColorPickerComponent } from './color-picker/color-picker.component';
 import { CalculatorComponent } from './calculator/calculator.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
--- ss2/bai_tap/fibonaci/src/app/color-picker/color-picker.component.ts
+++ ss2/bai_tap/fibonaci/src/app/color-picker/color-picker.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./color-picker.component.css']
 })
 export class ColorPickerComponent implements OnInit {
-
+  r = 0;
+  g = 0;
+  b = 0;
   constructor() { }
 
   ngOnInit(): void {
   }
+  rgb() {
+    const styles = {
+      'background-color': 'rgb(' + this.r + ',' + this.g + ',' + this.b + ')',
+    };
+    return styles;
 
+  }
 }
